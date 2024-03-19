9e28372914675183bfa37de1493f1658a49ebaa4
RubenD <rubendariopm14@gmail.com>
2024-03-19 16:41:39 -0400

    update

--- src/node.rs
+++ src/node.rs
@@ -1,4 +1,4 @@
-use heapless::Vec;
+use heapless::{String, Vec};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -8,15 +8,42 @@ pub enum Sensor {
   },
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ActuatorValue {
+  Light(bool),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Actuator {
+  /// Id, because there can actuators of the same kind
+  pub id: u8,
+  /// Value
+  pub value: ActuatorValue,
+}
+impl PartialEq for Actuator{
+  fn eq(&self, other: &Self) -> bool {
+      self.id == other.id
+  }
+}
+impl Eq for Actuator {}
+
 #[derive(Serialize, Deserialize,Debug)]
 pub enum Command {
   Info,
+  /// Set sensor reporting interval in ms
+  ReportingInterval(u16),
+  /// Set actuator
+  SetActuator(Actuator)
 }
 
-#[derive(Serialize, Deserialize,Debug)]
+#[derive(Serialize, Deserialize,Debug, Default)]
 pub enum Response {
-  Info{board_version: u8}
+  Info{board_version: u8},
+  #[default] Ok,
+  ErrActuatorNotFound,
+  ErrActuatorValueTypeDoesntMatch
 }
+
 
 pub const SENSORS_MAX:usize = 4;
 
