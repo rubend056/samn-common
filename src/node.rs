5b031f2b315653194db904638c65128d2eba3fa8
RubenD <rubendariopm14@gmail.com>
2024-03-20 10:57:51 -0400

    update

--- src/node.rs
+++ src/node.rs
@@ -3,17 +3,25 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Sensor {
-  Battery {
-    level: u8,
-  },
+  /// Battery level (in percentage 0-100)
+  /// 
+  /// For a 1.5v battery that has a range of 1 to 1.6v
+  /// being read by a microcontroller ADC of 10bits with a 3.3v power supply
+  /// The reading from ADC would be from ~300 to ~500, so `(adc - 300) / 2` would yield the percentage
+  Battery(u8),
+  /// Tempearature in farenheit
+  Temperature(u16),
+  /// Humidity in percentage
+  Humidity(u8)
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum ActuatorValue {
+  /// An on/off light
   Light(bool),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Actuator {
   /// Id, because there can actuators of the same kind
   pub id: u8,
@@ -38,13 +46,13 @@ pub enum Command {
 
 #[derive(Serialize, Deserialize,Debug, Default)]
 pub enum Response {
-  Info{board_version: u8},
+  Info{board_version: u8, actuators: Vec<Actuator, ACTUATORS_MAX>},
   #[default] Ok,
   ErrActuatorNotFound,
   ErrActuatorValueTypeDoesntMatch
 }
 
-
+pub const ACTUATORS_MAX:usize = 4;
 pub const SENSORS_MAX:usize = 4;
 
 
