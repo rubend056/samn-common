6e1d6aec5d13c6e5b4c094c4a89e68a8e094a690
RubenD <rubendariopm14@gmail.com>
2024-04-03 17:22:46 -0400

    making Payload address 16bits adding Add operations for Limbs

--- src/node.rs
+++ src/node.rs
@@ -1,96 +1,144 @@
 use heapless::{String, Vec};
 use serde::{Deserialize, Serialize};
 
-
-#[derive(Serialize, Deserialize,Clone,Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum Board {
-  /// For all samn boards <= 8
-  SamnV8,
-  SamnV9
+	/// For all samn boards <= 8
+	SamnV8,
+	SamnV9,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum Sensor {
-  /// Battery level (in percentage 0-100)
-  /// 
-  /// For a 1.5v battery that has a range of 1 to 1.6v
-  /// being read by a microcontroller ADC of 10bits with a 3.3v power supply
-  /// The reading from ADC would be from ~300 to ~500, so `(adc - 300) / 2` would yield the percentage
-  Battery(u8),
-  /// - Temperature in Celcius * 100
-  /// - Humidity in percentage
-  TempHum((i16, u8)),
+	/// Battery level (in percentage 0-100)
+	///
+	/// For a 1.5v battery that has a range of 1 to 1.6v
+	/// being read by a microcontroller ADC of 10bits with a 3.3v power supply
+	/// The reading from ADC would be from ~300 to ~500, so `(adc - 300) / 2` would yield the percentage
+	Battery(u8),
+	/// - Temperature in Celcius * 100
+	/// - Humidity in percentage
+	TempHum((i16, u8)),
+}
+
+impl core::ops::Add for Sensor {
+	type Output = Sensor;
+	fn add(self, rhs: Self) -> Self::Output {
+		match (self, rhs) {
+			(Self::Battery(level), Self::Battery(level_in)) => Self::Battery((level + level_in) / 2),
+			(Self::TempHum((temp, hum)), Self::TempHum((temp_in, hum_in))) => {
+				Self::TempHum(((temp + temp_in) / 2, (hum + hum_in) / 2))
+			}
+			_ => panic!("Can't add two different sensors"),
+		}
+	}
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum Actuator {
-  /// An on/off light
-  Light(bool),
+	/// An on/off light
+	Light(bool),
+}
+
+impl core::ops::Add for Actuator {
+	type Output = Actuator;
+	fn add(self, rhs: Self) -> Self::Output {
+		match (self, rhs) {
+			(Self::Light(on), Self::Light(on_in)) => Self::Light(on || on_in), // _ => panic!("Can't add two different actuators")
+		}
+	}
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub enum LimbType{Sensor{
-  /// The reporting interval (in seconds)
-  /// We will report on the closest value after wakeup.
-  report_interval: u16,
-  data: Option<Sensor> 
-}, Actuator(Actuator)}
+pub enum LimbType {
+	Sensor {
+		/// The reporting interval (in seconds)
+		/// We will report on the closest value after wakeup.
+		report_interval: u16,
+		data: Option<Sensor>,
+	},
+	Actuator(Actuator),
+}
+
+impl core::ops::Add for LimbType {
+	type Output = LimbType;
+	fn add(self, rhs: Self) -> Self::Output {
+		match (self, rhs) {
+			(
+				Self::Sensor {
+					report_interval: ri,
+					data: d,
+				},
+				Self::Sensor {
+					report_interval: ri_in,
+					data: d_in,
+				},
+			) => Self::Sensor {
+				report_interval: (ri + ri_in) / 2,
+				data: match (d, d_in) {
+					(None, None) => None,
+					(None, Some(s)) => Some(s),
+					(Some(s), None) => Some(s),
+					(Some(s), Some(s_in)) => Some(s + s_in),
+				},
+			},
+			(Self::Actuator(actuator), Self::Actuator(actuator_in)) => Self::Actuator(actuator + actuator_in),
+			_ => panic!("Can't add two different Limb Types"),
+		}
+	}
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct Limb(pub u16,pub LimbType);
+pub struct Limb(pub u16, pub LimbType);
 
-#[derive(Serialize, Deserialize,Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub enum Command {
-  /// Gets node Info
-  Info,
-  /// Get node Limb states
-  Limbs,
-  /// Set a limb
-  SetLimb(Limb)
+	/// Gets node Info
+	Info,
+	/// Get node Limb states
+	Limbs,
+	/// Set a limb
+	SetLimb(Limb),
 }
 
-#[derive(Serialize, Deserialize, Clone,Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct NodeInfo {
-  pub name: String::<12>,
-  pub board: Board
+	pub name: String<12>,
+	pub board: Board,
 }
 
-pub const LIMBS_MAX:usize = 4;
+pub const LIMBS_MAX: usize = 4;
 pub type Limbs = Vec<Limb, LIMBS_MAX>;
 
 #[repr(u8)]
-#[derive(Serialize, Deserialize,Debug, Default)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 pub enum Response {
-  #[default] Ok,
-  Info(NodeInfo),
-  Limbs(Limbs),
-  Heartbeat(u32),
-  ErrLimbNotFound=200,
-  ErrLimbTypeDoesntMatch
+	#[default]
+	Ok,
+	Info(NodeInfo),
+	Limbs(Limbs),
+	Heartbeat(u32),
+	ErrLimbNotFound = 200,
+	ErrLimbTypeDoesntMatch,
 }
 
-
-#[derive(Serialize, Deserialize,Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub enum MessageData {
-  Command{
-    /// What command id is this
-    id: u8,
-    command: Command
-  },
-  Response{
-    /// Which command id are we responding to
-    id: Option<u8>,
-    response: Response
-  },
+	Command {
+		/// What command id is this
+		id: u8,
+		command: Command,
+	},
+	Response {
+		/// Which command id are we responding to
+		id: Option<u8>,
+		response: Response,
+	},
 }
-
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Message {
-  /// From/for node id
-  pub id: u16,
-  pub data: MessageData
+	/// From/for node id
+	pub id: u16,
+	pub data: MessageData,
 }
-
-
-
--- src/radio/mod.rs
+++ src/radio/mod.rs
@@ -1,54 +1,55 @@
 /// Provides a trait for Radios to implement, so that we only use 1 API
-
 mod radios;
 
 pub trait Radio<E> {
-  fn transmit_(&mut self, payload:&Payload) -> Result<Option<bool>, E>;
-  fn receive_<P: embedded_hal::digital::InputPin>(&mut self, packet_ready_pin: &mut P) -> nb::Result<Payload, E>;
+	fn transmit_(&mut self, payload: &Payload) -> Result<Option<bool>, E>;
+	fn receive_<P: embedded_hal::digital::InputPin>(&mut self, packet_ready_pin: &mut P) -> nb::Result<Payload, E>;
 }
-
-
 
 #[derive(Default)]
 pub struct Payload([u8; 32]);
 impl Payload {
-    /// Copies the data to the payload
-    pub fn new(data: &[u8]) -> Self {
-        let mut s = Self::default();
-        if data.len() > s.0.len() - 1 {
-            panic!("Data too big for Payload");
-        }
-        s.0[0] = u8::try_from(data.len()).unwrap();
-        for (i, d) in data.iter().enumerate() {
-            s.0[i + 1] = *d;
-        }
-        s
-    }
-    pub fn new_with_addr(data: &[u8], address: u8) -> Self {
-        let mut s = Self::default();
-        if data.len() > s.0.len() - 2 {
-            panic!("Data too big for Payload");
-        }
-        s.0[0] = u8::try_from(data.len()).unwrap() | (1 << 7);
-        s.0[1] = address;
-        for (i, d) in data.iter().enumerate() {
-            s.0[i + 2] = *d;
-        }
-        s
-    }
-    pub fn address(&self) -> Option<u8> {
-        if (self.0[0] & (1 << 7)) > 0 {
-            Some(self.0[1])
-        } else {
-            None
-        }
-    }
-    /// Get the length of the packet
-    pub fn len(&self) -> u8 {
-        self.0[0] & !(1 << 7)
-    }
-    /// Get the data section of the packet
-    pub fn data(&self) -> &[u8] {
-        &self.0[1..(self.len() + 1) as usize]
-    }
+	/// Copies the data to the payload appending the length as the first byte
+	pub fn new(data: &[u8]) -> Self {
+		let mut s = Self::default();
+		if data.len() > s.0.len() - 1 {
+			panic!("Data too big for Payload");
+		}
+		s.0[0] = u8::try_from(data.len()).unwrap();
+		for (i, d) in data.iter().enumerate() {
+			s.0[i + 1] = *d;
+		}
+		s
+	}
+	/// Copies the data to the payload appending the length as the first byte, and address as 2nd and 3rd bytes
+	pub fn new_with_addr(data: &[u8], address: u16) -> Self {
+		let mut s = Self::default();
+		if data.len() > s.0.len() - 2 {
+			panic!("Data too big for Payload");
+		}
+		s.0[0] = u8::try_from(data.len()).unwrap() | (1 << 7);
+		let address_bytes = address.to_le_bytes();
+		s.0[1] = address_bytes[0];
+		s.0[2] = address_bytes[1];
+		for (i, d) in data.iter().enumerate() {
+			s.0[i + 3] = *d;
+		}
+		s
+	}
+	pub fn address(&self) -> Option<u16> {
+		if (self.0[0] & (1 << 7)) > 0 {
+			Some(u16::from_le_bytes(self.0[1..3].try_into().unwrap()))
+		} else {
+			None
+		}
+	}
+	/// Get the length of the packet
+	pub fn len(&self) -> usize {
+		(self.0[0] & !(1 << 7)).into()
+	}
+	/// Get the data section of the packet
+	pub fn data(&self) -> &[u8] {
+		let header_length = if self.address().is_some() { 3 } else { 1 } as usize;
+		&self.0[header_length..header_length + self.len()]
+	}
 }
